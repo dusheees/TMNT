b051aea9e9e4cd4762aa254c650aee56b1a320e2
Andrey Zhilin <komplayer31@yandex.ru>
2021-11-13 01:56:06 +0900

    Create Welcome View Controller and added constrains

---
+++ TMNT/WelcomeViewController.swift
@@ -0,0 +1,71 @@
+//
+//  WelcomeViewController.swift
+//  TMNT
+//
+//  Created by Андрей on 12.11.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    // MARK: - UIProperties
+    @IBOutlet var listOfImages: [UIImageView]!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var welcomeButton: UIButton!
+    // constrains
+    @IBOutlet var listOfConstrains: [NSLayoutConstraint]!
+    
+    
+    // MARK: - Properties
+    var size: CGSize!
+    var factor: CGFloat!
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        size = view.bounds.size
+        factor = min(size.width, size.height)
+        print("size = ", size.width)
+        
+        correctSize()
+    }
+    
+    func correctSize() {
+        for `image` in listOfImages {
+            `image`.transform = CGAffineTransform(scaleX: factor / 500, y: factor / 500)
+        }
+        
+        for `constraint` in listOfConstrains {
+            `constraint`.constant = valueForConstrains()
+        }
+        
+        welcomeLabel.font = UIFont.systemFont(ofSize: factor / 16)
+        welcomeButton.titleLabel?.font = UIFont.systemFont(ofSize: factor / 16)
+    }
+    
+    func valueForConstrains() -> CGFloat {
+        let value: CGFloat
+        let sizeValue: Double
+        
+        if size.width < size.height {
+            sizeValue = size.width
+        } else {
+            sizeValue = size.height
+        }
+        
+        switch sizeValue {
+        case 428.0... :
+            value = 120 + factor / 600
+        case 321.0 ... 428.0:
+            value = 20 + factor / 600
+        default:
+            value = 7 + factor / 600
+        }
+        
+        return value
+    }
+
+
+}
+
